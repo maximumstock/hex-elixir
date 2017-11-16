7c6046dea978f79530c470543e67a7bb506c3f73
Maximilian Stock <me@maximumstock.net>
2017-11-16 18:30:06 +0100

    Fix typo in mix.exs

--- apps/auction_indexer/mix.exs
+++ apps/auction_indexer/mix.exs
@@ -29,7 +29,7 @@ defmodule AuctionIndexer.Mixfile do
       {:distillery, "~> 1.5"},
       {:poison, "~> 3.1"},
       {:database, in_umbrella: true},
-      {:timex: "~> 3.1"}
+      {:timex, "~> 3.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true},
