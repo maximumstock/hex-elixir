dd71eb256eccb87e387525b9149263999f3d4ed3
Maximilian Stock <me@maximumstock.net>
2017-11-16 18:29:12 +0100

    Add Timex as dependency for auction indexer application

--- apps/auction_indexer/mix.exs
+++ apps/auction_indexer/mix.exs
@@ -18,7 +18,7 @@ defmodule AuctionIndexer.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :timex],
       mod: {AuctionIndexer.Application, []}
     ]
   end
@@ -28,7 +28,8 @@ defmodule AuctionIndexer.Mixfile do
     [
       {:distillery, "~> 1.5"},
       {:poison, "~> 3.1"},
-      {:database, in_umbrella: true}
+      {:database, in_umbrella: true},
+      {:timex: "~> 3.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true},
