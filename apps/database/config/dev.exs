20a3d7794ee95d46e4e0a4b41619cf2697b4e77f
Maximilian Stock <me@maximumstock.net>
2017-11-17 19:02:53 +0100

    Bring database into a solid state

--- apps/database/config/dev.exs
+++ apps/database/config/dev.exs
@@ -1,9 +1,2 @@
 use Mix.Config
 
-config :database, Database.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  database: "hex",
-  username: "hex",
-  password: "hex",
-  hostname: "localhost"
-
--- apps/database/lib/auction_message.ex
+++ apps/database/lib/auction_message.ex
@@ -6,22 +6,33 @@ defmodule Database.AuctionMessage do
 
   use Ecto.Schema, Timex
 
-  @fields [:id, :type, :events, :created_at]
+  @required_fields [:id, :type, :events, :created_at]
+  @optional_fiels  [:was_processed, :last_processed_at]
   @primary_key {:id, :string, autogenerate: false}
 
   schema "messages" do
     field :type, :string
     field :events, {:array, :map}
-    field :was_processed, :boolean, default: false
-    field :last_processed_at, :utc_datetime
     field :created_at, :utc_datetime
+    # optional
+    field :was_processed, :boolean, default: false
+    field :last_processed_at, :utc_datetime, default: nil
   end
 
   def to_changeset(struct, params \\ %{}) do
     struct
-    |> Ecto.Changeset.cast(params, @fields)
-    |> Ecto.Changeset.validate_required(@fields)
+    |> Ecto.Changeset.cast(params, @required_fields ++ @optional_fiels)
+    |> Ecto.Changeset.validate_required(@required_fields)
     |> Ecto.Changeset.unique_constraint(:id, [name: :messages_pkey])
+  end
+
+  def from_raw_message(raw_message) do
+    %__MODULE__{
+      id: raw_message["MessageId"],
+      type: raw_message["MessageType"],
+      created_at: parse_datetime(raw_message["MessageTime"]),
+      events: raw_message["Events"]
+    }
   end
 
   def parse_datetime(datetime) do
